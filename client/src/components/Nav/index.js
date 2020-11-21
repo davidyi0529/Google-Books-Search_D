ab3406d3af9fb9dc7101f15105ff7c7ef4e5bb52
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-11-20 18:00:51 -0800

    routing part 1

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-        React Reading List
+        Google Reading List
       </a>
+      <Link to="/">Books</Link>
+      <Link to="/detail">Detail</Link>
     </nav>
   );
 }
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -8,6 +8,44 @@ function Detail(props) {
     const [book, setBook] = useState([]);
 
     const { id } = useParams();
+    useEffect(() => {
+        API.getBook(id)
+            .then(res => 
+                setBook(res.data))
+            .catch(err => console.log(err));
+    }, []);
 
+    return(
+        <>
+        <Container fluid>
+            <Row>
+                <Col size="md-12">
+                <Jumbotron>
+                    <h1>{book.title} written by {book.author}</h1>
+                </Jumbotron>
+                </Col>
+            </Row>
 
+            <Row>
+                <Col size="md-6">
+                    <main>
+                        <h2>Description</h2>
+                        <p>{book.description}</p>
+                            <img src={book.link} alt={book.title}>  
+                            <a href={book.link}>{book.title}</a>
+                        </img>
+                    </main>
+                </Col>
+            </Row>
+
+            <Row>
+            <Col size="md-2">
+                <Link to="/">← Back to Books</Link>
+            </Col>
+            </Row>
+        </Container>
+        </>
+    );
 }
+
+export default Detail;
