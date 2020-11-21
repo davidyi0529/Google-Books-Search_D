b73a83b0ff50e6e0e9a99f641ea183b4128e969c
David Yi <david.yi.0529@gmail.com>
2020-11-21 12:06:31 -0800

    updated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
@@ -15,30 +14,6 @@ function App() {
         </Switch>
       </div>
     </Router>
-=======
-import Books from "./pages/Books";
-import Detail from "./pages/Detail"
-import Nav from "./components/Nav";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-function App() {
-  return (
-    <div>
-      <Router>
-        <Nav />
-        <Switch>
-          <Route exact path="/">
-            <Books />
-          </Route>
-        </Switch>
-        <Switch>
-          <Route exact path="/detail">
-            <Detail />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
->>>>>>> 028410c3fad209ffae71ef69f593916adce7eede
   );
 }
 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { NavLink } from "react-router-dom";
 import "./style.css";
 
@@ -29,37 +28,5 @@ function Nav() {
         </header>
     );
 };
-=======
-import { Link } from "react-router-dom";
-
-var style = {
-  backgroundColor: "#BBBBBB",
-  borderTop: "1px solid #E7E7E7",
-  textAlign: "right",
-  left: "0",
-  bottom: "0",
-  height: "80px",
-  width: "100%",
-}
-
-function Nav() {
-  return (
-    <nav className="navbar-expand-lg">
-
-        <div>
-          <ul className="navbar-nav ml-auto w-100 justify-content-end" style={style}>
-            <li className="nav-item">
-              <Link to="/">Books</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/detail">Detail</Link>
-            </li>
-        
-          </ul>
-        </div>
-    </nav>
-  );
-}
->>>>>>> 028410c3fad209ffae71ef69f593916adce7eede
 
 export default Nav;
